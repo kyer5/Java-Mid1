5c0d411b7dc38c7f7dfde993691a3abf021cdf6b
강예린 <jjykhs0505@naver.com>
2024-08-05 21:56:23 +0900

    section2. 문제 풀이

---
+++ src/lang/immutable/test/ImmutableMyDateMain.java
@@ -0,0 +1,16 @@
+package lang.immutable.test;
+
+public class ImmutableMyDateMain {
+
+    public static void main(String[] args) {
+        ImmutableMyDate immutableMyDate1 = new ImmutableMyDate(2003, 5, 5);
+        ImmutableMyDate immutableMyDate2 = immutableMyDate1;
+        System.out.println("immutableMyDate1 = " + immutableMyDate1);
+        System.out.println("immutableMyDate2 = " + immutableMyDate2);
+
+        System.out.println("2025 -> date1");
+        immutableMyDate1 = immutableMyDate1.withYear(2025);
+        System.out.println("immutableMyDate1 = " + immutableMyDate1);
+        System.out.println("immutableMyDate2 = " + immutableMyDate2);
+    }
+}
